6d6dde1c68c74dba2576088aeb9a69f7dccaf290
ilmoi <iljamoi@protonmail.com>
2021-10-28 20:16:49 +0300

    cpi works

--- programs/anchor_play_3/src/lib.rs
+++ programs/anchor_play_3/src/lib.rs
@@ -1,13 +1,19 @@
 use anchor_lang::prelude::*;
+use puppet::cpi::accounts::SetData;
+use puppet::program::Puppet;
+use puppet::{self, Data};
 
 declare_id!("BPvPwaQK28fsMHNJLf4i9jcsaA258tXGzrEaNQWRVJE5");
+
+// ============================================================================= logic
 
 #[program]
 pub mod anchor_play_3 {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>, data: u64) -> ProgramResult {
+    pub fn initialize(ctx: Context<Initialize>, data: u64, authority: Pubkey) -> ProgramResult {
         let data_holder = &mut ctx.accounts.passed_data_holder_acc;
+        data_holder.auth = authority;
         data_holder.data = data;
         Ok(())
     }
@@ -17,13 +23,22 @@ pub mod anchor_play_3 {
         data_holder.data = data;
         Ok(())
     }
+
+    pub fn pull_strings(ctx: Context<PullStrings>, data: u64) -> ProgramResult {
+        let cpi_program = ctx.accounts.puppet_program.to_account_info();
+        let cpi_accounts = SetData {
+            puppet: ctx.accounts.puppet.to_account_info(),
+        };
+        let cpi_ctx = CpiContext::new(cpi_program, cpi_accounts);
+        puppet::cpi::set_data(cpi_ctx, data)
+    }
 }
 
 // ============================================================================= instructions
 
 #[derive(Accounts)]
 pub struct Initialize<'info> {
-    #[account(init, payer = passed_user_acc, space = 8 + 8)]
+    #[account(init, payer = passed_user_acc, space = 8 + 8 + 32)]
     pub passed_data_holder_acc: Account<'info, DataHolder>,
     #[account(mut)]
     pub passed_user_acc: Signer<'info>,
@@ -32,8 +47,16 @@ pub struct Initialize<'info> {
 
 #[derive(Accounts)]
 pub struct Update<'info> {
-    #[account(mut)]
+    #[account(mut, has_one = auth)]
     pub passed_data_holder_acc: Account<'info, DataHolder>,
+    pub auth: Signer<'info>,
+}
+
+#[derive(Accounts)]
+pub struct PullStrings<'info> {
+    #[account(mut)]
+    pub puppet: Account<'info, Data>,
+    pub puppet_program: Program<'info, Puppet>,
 }
 
 // ============================================================================= accounts
@@ -41,5 +64,6 @@ pub struct Update<'info> {
 #[account]
 pub struct DataHolder {
     pub data: u64,
+    pub auth: Pubkey,
 }
 
--- tests/anchor_play_3.ts
+++ tests/anchor_play_3.ts
@@ -1,19 +1,45 @@
 import * as anchor from '@project-serum/anchor';
 import { Program } from '@project-serum/anchor';
 import { AnchorPlay3 } from '../target/types/anchor_play_3';
-import assert from 'assert'
+import { Puppet } from '../target/types/puppet';
+import assert from 'assert';
 
 describe('anchor_play_3', () => {
-
   const provider = anchor.Provider.env();
   anchor.setProvider(provider);
 
   const dataHolder = anchor.web3.Keypair.generate();
-
+  const authority = anchor.web3.Keypair.generate();
   const program = anchor.workspace.AnchorPlay3 as Program<AnchorPlay3>;
 
+  const puppetDataHolder = anchor.web3.Keypair.generate();
+  const puppet_program = anchor.workspace.Puppet as Program<Puppet>;
+
+  it('does cpi ok', async () => {
+    const tx1 = await puppet_program.rpc.initialize({
+      accounts: {
+        puppet: puppetDataHolder.publicKey,
+        user: provider.wallet.publicKey,
+        systemProgram: anchor.web3.SystemProgram.programId,
+      },
+      signers: [puppetDataHolder],
+    });
+    console.log("Your transaction 1 signature", tx1);
+
+    const tx2 = await program.rpc.pullStrings(new anchor.BN(123), {
+      accounts: {
+        puppet: puppetDataHolder.publicKey,
+        puppetProgram: puppet_program.programId,
+      },
+    });
+    console.log("Your transaction 2 signature", tx2);
+
+    const account = await puppet_program.account.data.fetch(puppetDataHolder.publicKey);
+    assert.equal(account.data.toString(), '123');
+  })
+
   it('Is initialized!', async () => {
-    const tx = await program.rpc.initialize(new anchor.BN(1234), {
+    const tx = await program.rpc.initialize(new anchor.BN(1234), authority.publicKey, {
       accounts: {
         passedDataHolderAcc: dataHolder.publicKey,
         passedUserAcc: provider.wallet.publicKey,
@@ -32,7 +58,9 @@ describe('anchor_play_3', () => {
     const tx = await program.rpc.update(new anchor.BN(4444), {
       accounts: {
         passedDataHolderAcc: dataHolder.publicKey,
+        auth: authority.publicKey,
       },
+      signers: [authority]
     });
     console.log('sig is ', tx);
 
